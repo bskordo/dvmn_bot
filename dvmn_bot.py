a5abccb14c3f1f4c167242d2876c456c59eee670
bskordo <samat.study@gmail.com>
2020-04-30 18:56:36 +0600

    Fix list issue,rename env values,move logging settings under main

--- dvmn_bot.py
+++ dvmn_bot.py
@@ -10,26 +10,25 @@ ATTEMPS_COUNT = 2
 SLEEPING_TIME = 5
 TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
 NOTIFICATION_TELEGRAM_TOKEN = os.environ['NOTIFICATION_TELEGRAM_TOKEN']
-AUTHORIZATION_TOKEN = os.environ['AUTHORIZATION_TOKEN']
-CHAT_ID = os.environ['CHAT_ID']
+DVMN_AUTHORIZATION_TOKEN = os.environ['DVMN_AUTHORIZATION_TOKEN']
+TELEGRAM_USER_CHAT_ID = os.environ['TELEGRAM_USER_CHAT_ID']
 TIMEOUT = 60
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 notification_bot = telebot.TeleBot(NOTIFICATION_TELEGRAM_TOKEN)
 logger = logging.getLogger('Logger')
-logger.setLevel(logging.WARNING)
-logger.addHandler(TelegramLogsHandler(notification_bot, CHAT_ID))
+
 
 
 def send_result_message(messages):
     positive_message = 'Все хорошо, можно браться за следeущее задание'
     negative_message = "Есть ошибки, надо исправить"
-    result_message = negative_message if messages[-1]['is_negative'] else positive_message
+    result_message = negative_message if messages[0]['is_negative'] else positive_message
     feed_back_message = 'Mы проверили Ваше задание "{}" \n {}'.format(messages[0]['lesson_title'], result_message)
-    bot.send_message(chat_id=CHAT_ID, text=feed_back_message)
+    bot.send_message(chat_id=TELEGRAM_USER_CHAT_ID, text=feed_back_message)
 
 
 def is_server_response_correct(response):
-    return response.status_code == 200 and 'error' not in response.json()
+    return response.ok and 'error' not in response.json()
 
 
 def get_status_and_timestamp(json_response):
@@ -45,7 +44,7 @@ def get_response_from_server():
     timestamp = None
     connection_attempt = 0
     url = "https://dvmn.org/api/long_polling/"
-    headers = {"Authorization": AUTHORIZATION_TOKEN}
+    headers = {"Authorization": DVMN_AUTHORIZATION_TOKEN}
     while True:
         payload = {'timestamp': timestamp}
         try:
@@ -69,4 +68,6 @@ def get_response_from_server():
             logger.error('Bot is broken by error: {}'.format(error_msg))
 
 if __name__ == '__main__':
+    logger.setLevel(logging.WARNING)
+    logger.addHandler(TelegramLogsHandler(notification_bot, TELEGRAM_USER_CHAT_ID))
     get_response_from_server()
